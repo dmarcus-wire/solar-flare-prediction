e4692c2d1851efcaa7939bd3695dafc6493ba482
dmarcus-wire <dmarcus@redhat.com>
2024-03-24 21:43:52 -0600

    update README

--- notebooks/setup.sh
+++ notebooks/setup.sh
@@ -5,6 +5,16 @@
 ENV=solar-flare
 VERS=3.12
 
+# install miniconda
+mkdir -p ~/miniconda3
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
+bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
+rm -rf ~/miniconda3/miniconda.sh
+
+# initialize newly-installed Miniconda
+~/miniconda3/bin/conda init bash
+~/miniconda3/bin/conda init zsh
+
 echo "update conda"
 conda update -n base -c defaults conda -y
 
